10e4bfc6035e46dc1285adbc44578ef8d62499ed
Cristina Lucin <cristinajlucin@gmail.com>
2022-09-13 10:05:32 -0500

    Added corrected join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -118,58 +118,56 @@ ORDER BY department_number
 
 # 7
 /*
-SELECT d.dept_no AS department_number,
-d.dept_name AS  department_name,
-COUNT(de.emp_no) AS num_employees,
-AVG(s.salary) AS average_salary
-FROM departments as d
-JOIN dept_emp as de
-ON d.dept_no = de.dept_no
-JOIN salaries as s
-ON de.emp_no = s.emp_no
-WHERE de.to_date = '9999-01-01'
-	AND s.to_date = '9999-01-01'
-GROUP BY department_number
-ORDER BY average_salary
+SELECT d.dept_name department_name, AVG(s.salary) salary
+FROM dept_emp de
+JOIN salaries s ON de.emp_no = s.emp_no
+	AND de.to_date > CURDATE()
+    AND s.to_date > CURDATE()
+JOIN departments d ON de.dept_no = d.dept_no
+GROUP BY department_name
+ORDER BY salary DESC
+LIMIT 1
 */
 
 #8 Who is the highest paid employee in the Marketing department?
 /*
-SELECT e.first_name AS first_name, e.last_name AS last_name, s.salary AS salary
-FROM employees AS e
-JOIN salaries as s
-ON e.emp_no = s.emp_no
-JOIN dept_emp AS de
-ON s.emp_no = de.emp_no
-JOIN departments as d
-ON de.dept_no = d.dept_no
-WHERE d.dept_no = 'd001'
+USE employees;
+
+SELECT e.first_name first_name, e.last_name last_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+	AND de.to_date > CURDATE()
+JOIN salaries s ON e.emp_no = s.emp_no
+	AND s.to_date > CURDATE()
+JOIN departments d ON de.dept_no = d.dept_no
+	AND d.dept_name = 'Marketing'
 ORDER BY s.salary DESC
+LIMIT 1
+
 */
 
 #9 Which current department manager has the highest salary?
 
 /*
-SELECT e.first_name AS first_name,e.last_name AS last_name, s.salary AS salary, d.dept_name AS dept_name
-FROM employees as e
-JOIN salaries as s
-ON e.emp_no = s.emp_no
-JOIN dept_manager as dm
-ON s.emp_no = dm.emp_no
-JOIN departments as d
-ON dm.dept_no = d.dept_no
-WHERE dm.to_date = '9999-01-01'
-	AND s.to_date = '9999-01-01'
+USE employees;
+
+SELECT e.first_name first_name, e.last_name last_name, s.salary salary, d.dept_name department_name
+FROM employees e
+JOIN dept_manager dm ON e.emp_no = dm.emp_no
+	AND dm.to_date > CURDATE()
+JOIN salaries s ON e.emp_no = s.emp_no
+	AND s.to_date > CURDATE()
+JOIN departments d ON dm.dept_no = d.dept_no
+ORDER BY s.salary DESC
+LIMIT 1
 */
 
 #10 Determine the average salary for each department. Use all salary information and round your results
 /*
-SELECT d.dept_name AS department_Name, ROUND(AVG(s.salary)) AS average_salary
-FROM salaries as s
-JOIN dept_emp as de
-ON s.emp_no = de.emp_no
-JOIN departments as d
-ON de.dept_no = d.dept_no
+SELECT d.dept_name department_Name, ROUND(AVG(s.salary)) average_salary
+FROM salaries s
+JOIN dept_emp de ON s.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
 GROUP BY department_Name
 ORDER BY average_salary DESC
 */
