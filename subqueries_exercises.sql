f6fb649420d8238ed5259339265c238b79be6964
Cristina Lucin <cristinajlucin@gmail.com>
2022-09-13 16:46:45 -0500

    Updated subqueries exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -3,101 +3,91 @@ USE employees;
 #1. Find all the current employees with the same hire date as employee 101010 using a sub-query.
 /*
 SELECT *
-FROM employees e1
-JOIN dept_empt de ON e1.emp_no = de.emp_no
-	AND de.to_date > CURDATE()
-WHERE e1.hire_date = (
-						SELECT e.hire_date
-						FROM employees e
-						WHERE e.emp_no = 101010
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+WHERE to_date > CURDATE()
+AND hire_date =
+						(SELECT hire_date
+						FROM employees 
+						WHERE emp_no = 101010)
 							
 					)
 */
 
 /*
 #2 Find all the titles ever held by all current employees with the first name Aamod.
-USE employees;
 
 SELECT DISTINCT title
 FROM titles
-WHERE emp_no IN	(	
-			
+WHERE to_date > CURDATE()
+AND emp_no IN	(	
             SELECT emp_no
 			FROM employees
 			WHERE first_name = 'Aamod'
-		)
-	AND to_date > CURDATE()
-
+		)		)
 */
 
 /*
-#3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
-
 USE employees;
-
-SELECT *
+SELECT COUNT(*)
 FROM employees
-WHERE emp_no IN (
-				SELECT emp_no
-				FROM dept_emp
-				WHERE to_date < CURDATE()
-                )
+WHERE emp_no NOT IN (
+					SELECT emp_no
+                    FROM dept_emp
+                    WHERE to_date LIKE '9999%'
 */
 
 /*
 # 4. Find all the current department managers that are female. List their names in a comment in your code.
 
 USE employees;
+/*
+#4 Find all the current department managers that are female. List their names in a comment in your code.
 
-SELECT CONCAT(first_name, ' ', last_name) AS full_name
-FROM employees
+SELECT first_name, last_name
+from employees
 WHERE gender = 'F'
-	AND emp_no IN (
-		SELECT emp_no
-		FROM dept_manager dm
-		)
-        
-# 'Isamu Legleitner'
-/*'Shirish Ossenbruggen'
-'Karsten Sigstam'
-'Krassimir Wegerle'
-'Rosine Cools'
-'Leon DasSarma'
-'Peternela Onuegbe'
-'Rutger Hofmeyr'
-'Sanjoy Quadeer'
-'Hilary Kambil'
-'Tonny Butterworth'
-'Marjo Giarratana'
-'Xiaobin Spinelli'
+AND emp_no IN (
+				SELECT emp_no
+                FROM dept_manager
+                WHERE to_date > CURDATE()
+                )
+                
+# 'Isamu','Legleitner' 'Karsten','Sigstam''Leon','DasSarma' 'Hilary','Kambil'
+
+
 */
 
 #5
 /*
+#5 Find all the employees who currently have a higher salary than the companies overall, historical average salary.
 SELECT *
-FROM employees 
+FROM employees
 WHERE emp_no IN (
-		SELECT emp_no
-				FROM salaries
-				WHERE salary > (SELECT AVG(salary) FROM salaries)
-					AND emp_no IN (
-							SELECT emp_no
-							FROM dept_emp
-							WHERE to_date > CURDATE()
-									)
-					AND to_date > CURDATE()
-				);		
+					SELECT emp_no
+                    FROM salaries
+                    WHERE to_date > CURDATE()
+                    AND salary > (
+									SELECT AVG(SALARY)
+                                    FROM salaries
+#154543
 */
 
-/*8 How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built 
-in function to calculate the standard deviation.) What percentage of all salaries is this? (78)
+/*6 How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built 
+in function to calculate the standard deviation.) What percentage of all salaries is this?
+
 USE employees;
 
-SELECT * FROM salaries
+SELECT (
+	SELECT
+	COUNT(*)
+    FROM salaries
     WHERE to_date > CURDATE() 
-    AND salary >= (
-					(SELECT MAX(salary) FROM salaries) - (SELECT ROUND(STD(salary)) FROM salaries))
-    AND salary <= (SELECT MAX(salary) FROM salaries) 
+	    AND 
+    salary >= ((SELECT MAX(salary) FROM salaries WHERE to_date > CURDATE()) 
+    - (SELECT STD(salary) FROM salaries WHERE to_date > CURDATE())))
+    
+ / (SELECT COUNT(*) FROM salaries WHERE to_date > CURDATE()) * 100;
     
 /*
 BONUS 1
